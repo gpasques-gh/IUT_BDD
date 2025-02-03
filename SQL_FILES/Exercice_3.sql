46ab0cd214b6654b74d09d7a1fe7493d42528067
gpasques-gh <germain.pasques4@etu.univ-lorraine.fr>
2025-02-03 11:20:37 +0100

    Corrections

--- SQL_FILES/Exercice_3.sql
+++ SQL_FILES/Exercice_3.sql
@@ -109,7 +109,87 @@ begin
 
 end;
 
+-- EXERCICE 34
 
+declare
+
+    cursor c_pays is
+        select distinct paysfilm
+        from FILM;
+
+    cursor c_film (p_pays film.paysfilm%type) is
+        select titre, datesortie, idfilm
+        from film
+        where paysfilm = p_pays;
+
+    cursor c_acteur (id_f film.idfilm%type) is
+        select nom, prenom
+        from acteur inner join jouer on acteur.idacteur = jouer.idacteur
+        where id_f = idfilm
+        order by nom;
+
+begin
+
+    for r_pays in c_pays loop
+        DBMS_OUTPUT.PUT_LINE('  Films de ' || r_pays.PAYSFILM);
+        for r_film in c_film (r_pays.PAYSFILM) loop
+        DBMS_OUTPUT.PUT_LINE('      Acteurs ayant joué dans ' || r_film.TITRE || ' sorti le ' || r_film.DATESORTIE);
+            for r_acteur in c_acteur(r_film.IDFILM) loop
+                DBMS_OUTPUT.PUT_LINE(' - ' || r_acteur.PRENOM || ' ' || r_acteur.NOM);
+            end loop;
+        end loop;
+    end loop;
+
+
+end;
+
+
+declare
+
+    v_moy number(4,2);
+    v_nb_notes number(3,0);
+
+    cursor c_pays_int is
+    select distinct PAYSINT
+    from INTERNAUTE
+    order by PAYSINT;
+
+    cursor c_int (p_pays internaute.PAYSINT%type) is
+    select nomint, prenomint, email
+    from INTERNAUTE
+    where PAYSINT = p_pays
+    order by prenomint, nomint;
+
+    cursor c_film (p_email internaute.email%type) is
+    select titre, note
+    from notation inner join film on notation.idfilm = film.idfilm
+    where email = p_email
+    order by titre;
+
+begin
+
+
+
+    for r_pays in c_pays_int loop
+        DBMS_OUTPUT.PUT_LINE('Internautes venant du pays ' || r_pays.PAYSINT || ' : ');
+        for r_int in c_int(r_pays.PAYSINT) loop
+            select count(note), avg(note) into v_nb_notes, v_moy
+            from notation
+            where email = r_int.EMAIL;
+
+            if v_nb_notes > 0 then
+                DBMS_OUTPUT.PUT_LINE('  - ' || r_int.PRENOMINT || ' ' || r_int.NOMINT || ' (' || r_int.EMAIL || ') a noté ' || v_nb_notes || ' films pour une moyenne de ' || v_moy || ', en voici la liste : ');
+                for r_film in c_film(r_int.EMAIL) loop
+                    DBMS_OUTPUT.PUT_LINE('    - ' || r_film.TITRE || ' (' || r_film.NOTE || ')');
+                end loop;
+            else
+                DBMS_OUTPUT.PUT_LINE('  - ' || r_int.PRENOMINT || ' ' || r_int.NOMINT || ' (' || r_int.EMAIL || ') n a noté aucun film');
+            end if;
+
+        end loop;
+    end loop;
+
+end;
 
 
 
